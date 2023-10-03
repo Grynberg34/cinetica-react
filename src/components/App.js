01824fdab0f49c9ef6aba5f5ffcb39e1268201e0
Francisco Bignotto <franciscogrynberg34@gmail.com>
2023-10-03 14:55:38 -0300

    v0.0.1 estrutura

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import { store } from '../store';
+import { Navigate } from "react-router-dom";
+
+function App(props) {
+
+
+
+  
+}
+
+function mapStateToProps(state) {
+  return {
+
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(App);
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import App from './components/App';
+import {store} from './store.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
+
+  </Provider>,
+  document.querySelector('#root')
+);
+
+
