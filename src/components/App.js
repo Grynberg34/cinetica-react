3fafb80f2f67e48ff726934eea7ba945f4c360cc
Francisco Bignotto <franciscogrynberg34@gmail.com>
2023-10-03 17:04:41 -0300

    v0.0.2 header desktop

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import { connect } from 'react-redux';
 import { store } from '../store';
 import { Navigate } from "react-router-dom";
+import Header from './Header';
+import "../scss/app.scss";
 
 function App(props) {
 
 
+  return (
+    <div id="home">
+      <div className="home">
 
+        <Header></Header>
+
+      </div>
+    </div>
+  )
   
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,124 @@
-import { connect } from 'react-redux';
-import { store } from '../store';
-import { Navigate } from "react-router-dom";
+import "../scss/header.scss";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from "react-router-dom";
 
-function App(props) {
+function Header() {
+
+  return (
+    <div id="header">
+
+      <div className="header">
+        <Container fluid>
+            <Row>
+              <Col md={2}>
+                <img className='header__logo' src="/images/header/logo.svg" alt="" />
+              </Col>
+
+              <Col md={1}></Col>
+
+              <Col md={6}>
+
+                <div className="header__menu">
+
+                  <Container fluid>
+                    <Row>
+
+                      <Col md={3}>
+                        
+                        <Link className="header__menu__link" to="/">Circuito</Link>
+
+                      </Col>
+
+                      <Col md={3}>
+                        
+                        <Link className="header__menu__link" to="/">Pauta</Link>
+
+                      </Col>
 
 
+                      <Col md={3}>
+                        
+                        <Link className="header__menu__link" to="/">Cobertura</Link>
+
+                      </Col>
+
+                      <Col md={3}>
+                        
+                        <Link className="header__menu__link" to="/">Biblioteca</Link>
+
+                      </Col>
+
+                    </Row>
+                  
+                </Container>
+
+                </div>
+
+                <div className="header__sub">
+
+                  <Container fluid>
+                    <Row>
+
+                      <Col md={3}>
+
+                        <Link to="/"><img className="header__sub__lens" src="/images/header/lupa.svg" alt="" /></Link>
+                        
+                      </Col>
+
+                      <Col md={3}>
+                        <Link className="header__sub__link" to="/">Categorias</Link>
+                      </Col>
+
+                      <Col md={3}>
+                        <Link className="header__sub__link" to="/">Tags</Link>
+                      </Col>
+
+                      <Col md={3}>
+                        <Link className="header__sub__link" to="/">Autores</Link>
+                      </Col>
+
+                    </Row>
+                  </Container>
+
+                </div>
+
+
+              </Col>
+
+              <Col md={1}></Col>
+
+              <Col md={2}>
+
+                  <div className="header__social">
+                  <Container fluid>
+                    <Row>
+                      <Col md={4}>
+                        <a target="_blank" rel="noreferrer" href="/"><img className="header__social__img" src="/images/header/instagram.svg" alt="" /></a>
+                      </Col>
+
+                      <Col md={4}>
+                        <a target="_blank" rel="noreferrer" href="/"><img className="header__social__img" src="/images/header/facebook.svg" alt="" /></a>
+                      </Col>
+
+                      <Col md={4}>
+                        <a target="_blank" rel="noreferrer" href="/"><img className="header__social__img" src="/images/header/twitter.svg" alt="" /></a>
+                      </Col>
+                    </Row>
+                  </Container>
+                  </div>
+
+              </Col>
+            </Row>
+          </Container>
+
+    </div>
+
+    </div>
+  )
 
   
 }
 
-function mapStateToProps(state) {
-  return {
-
-  }
-}
-
-export default connect(
-  mapStateToProps
-)(App);
+export default Header;
